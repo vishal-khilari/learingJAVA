da24e3c3c8ec3a54601ea506ab93169186bd4d2a
vishal khilari <vishalkhilari0@gmail.com>
2024-09-05 21:49:48 +0530

    Length

--- practice.java
+++ practice.java
@@ -1,12 +1,6 @@
 class practice {
     public static void main(String args[]) {
         String name = "vishal";
-        String friend = "binod";
-        int age = 19;
-        String neighbour = friend;
-        System.out.println(name);
-        System.out.println(friend);
-        System.out.println(neighbour);
-        System.out.println(age);
+        System.out.println(name.length());
     }
 }
