7a5cb5b38fb9f88e1eea09208cdf90549f5a426f
vishal khilari <vishalkhilari0@gmail.com>
2024-09-05 21:55:41 +0530

    concatenate

--- practice.java
+++ practice.java
@@ -1,6 +1,8 @@
 class practice {
-    public static void main(String args[]) {
-        String name = "vishal";
-        System.out.println(name.length());
+    public static void main(String[] args) {
+        String name1 = "Vishal ";
+        String name2 = "Khilari";
+        String name3 = name1+name2;
+        System.out.println(name3);
     }
 }
