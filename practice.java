eb15ff63cf357a29552ff97e934f2c2e69447bd6
vishal khilari <vishalkhilari0@gmail.com>
2024-09-07 19:18:09 +0530

    Array assending order

--- practice.java
+++ practice.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
+
 class  practice{
     public static void main(String[] args){
         int[] marks = new int[3];
         marks[0] = 96;
         marks[1] = 98;
         marks[2] = 90;
+        System.out.println(marks[0]); //before assending order
+        Arrays.sort(marks);  //makes assending order
         System.out.println(marks.length);
+        System.out.println(marks[0]); //after assending order
     }
 }
